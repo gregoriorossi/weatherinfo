174c9978960aaa1b51c884a88ce64713ea502664
Rossi Gregorio <gr.rossi@reply.it>
2024-02-29 08:31:37 +0100

    app - implemented call to api to retrieve the locations

--- weatherinfoapp/src/components/searchbox.tsx
+++ weatherinfoapp/src/components/searchbox.tsx
@@ -1,12 +1,51 @@
-import React, { useState, useEffect, Component } from 'react';
+import React, { Component } from 'react';
+import { ChangeEvent } from 'react';
 import { Button, Form } from 'react-bootstrap';
-import { ISearchLocationByTextResponse } from '../models/weatherInfo.models';
+import { ISearchLocationByTextResponse, ISearchResultModel } from '../models/weatherInfo.models';
 
 
 type SearchBoxState = {
-	
+	Results: ISearchResultModel[];
 }
 export class SearchBox extends Component<{}, SearchBoxState> {
+
+	componentWillMount() {
+		this.setState({
+			Results: []
+		});
+	}
+
+	onTextInputChange = (e: React.ChangeEvent<HTMLInputElement>): void => {
+
+		const text: string = e.target.value;
+		const url = 'https://localhost:7177/weatherForecast/searchlocationBytext/{text}'
+			.replace('{text}', text);
+
+		fetch(url)
+			.then((response) => response.json())
+			.then((data: ISearchLocationByTextResponse[]) => {
+				console.log(data);
+				this.buildResults(data);
+			})
+			.catch((err) => {
+				console.log(err.message);
+			});
+	}
+
+	private buildResults(response: ISearchLocationByTextResponse[]): void {
+
+		const result: ISearchResultModel[] = response.map((x): ISearchResultModel => ({
+			Text: x.name,
+			IsFavorite: false,
+			Lat: x.lat,
+			Long: x.lon
+		}));
+
+		this.setState({
+			Results: result
+		});
+	}
+
 
 	render() {
 		return (
@@ -14,69 +53,12 @@ export class SearchBox extends Component<{}, SearchBoxState> {
 				<Form.Control
 					type="search"
 					placeholder="Search"
+					onChange={this.onTextInputChange}
 					className="me-2"
 					aria-label="Search" />
-				<Button variant="outline-success" > Search </Button>
+				<Button variant="outline-success">Search</Button>
 			</Form>
 		);
 	}
 
 }
-
-//export class Clock extends Component<{}, {}> {
-
-//    // The tick function sets the current state. TypeScript will let us know
-//    // which ones we are allowed to set.
-//    tick() {
-//        this.setState({
-//            time: new Date()
-//        });
-//    }
-
-//    // Before the component mounts, we initialise our state
-//    componentWillMount() {
-//        this.tick();
-//    }
-
-//    // After the component did mount, we set the state each second.
-//    componentDidMount() {
-//        setInterval(() => this.tick(), 1000);
-//    }
-
-//    // render will know everything!
-//    render() {
-//        return <p>The current time is test</p>
-//    }
-//}
-
-//export default class SearchBox extends Component {
-
-//	const [suggestions, favorites] = useState([]);
-
-//	//useEffect(() => {
-//	//	fetch('https://localhost:7177/weatherForecast/searchlocationBytext/udin')
-//	//		.then((response) => response.json())
-//	//		.then((data: ISearchLocationByTextResponse) => {
-// //               console.log(data);
-// //               // setPosts(data);
-// //           })
-//	//		.catch((err) => {
-//	//			console.log(err.message);
-//	//		});
-//	//}, []);
-
-//	render() {
-//		return (
-//			<Form className= "d-flex" >
-//					<Control
-//					  type="search"
-//				placeholder = "Search"
-//				className = "me-2"
-//				aria - label="Search"
-//					/>
-//					<Button variant="outline-success" > Search < /Button>
-//						< /Form>
-//			);
-//	}
-
-//};
