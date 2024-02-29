24e5638d4b83c2cbb282fa086c9a1478f46feb51
Rossi Gregorio <gr.rossi@reply.it>
2024-02-29 08:10:51 +0100

    app - added typescript and tsx

---
+++ weatherinfoapp/src/App.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Component } from 'react';
+import './App.css';
+import { Header } from './components/header';
+import { SearchBox } from './components/searchbox';
+
+export class App extends Component<{}, {}> {
+    render() {
+        return (
+            <div className="container">
+                <div className="row">
+                    <Header></Header>
+                    <SearchBox></SearchBox>
+                </div>
+            </div>
+
+        );
+    }
+   
+}
+
+export default App;
